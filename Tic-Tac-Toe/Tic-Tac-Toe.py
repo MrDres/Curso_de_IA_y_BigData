c3bcacb6f0ce0c4210bb18d5bd6f23762f55774f
MrDres <dres@mundo-r.com>
2023-12-30 15:16:29 +0100

    Reorganizando Carpetas

---
+++ Tic-Tac-Toe/Tic-Tac-Toe.py
@@ -0,0 +1,23 @@
+print("Bienvenido al campeonato mundial de tres en raya\n")
+pos1="1"
+pos2="2"
+pos3="3"
+pos4="4"
+pos5="5"
+pos6="6"
+pos7="7"
+pos8="8"
+pos9="9"
+print("     +-------+-------+-------+")
+print("     |       |       |       |")
+print("     |  ",pos1,"  |  ",pos2,"  |  ",pos3,"  |")
+print("     |       |       |       |")
+print("     +-------+-------+-------+")
+print("     |       |       |       |")
+print("     |  ",pos3,"  |  ",pos4,"  |  ",pos5,"  |")
+print("     |       |       |       |")
+print("     +-------+-------+-------+")
+print("     |       |       |       |")
+print("     |  ",pos7,"  |  ",pos8,"  |  ",pos9,"  |")
+print("     |       |       |       |")
+print("     +-------+-------+-------+")
